53d60fb8cd6e08dac14aba3d7ca594c6e412eb20
Nikita Yutanov <nikitayutanov@gmail.com>
2021-01-18 21:39:38 +0500

    Fix multiple function executions on one control key press

--- js/control.js
+++ js/control.js
@@ -3,22 +3,40 @@
 import { snake } from './snake.js';
 import { game } from './game.js';
 
+function eventHandler(e) {
+    if (
+        (e.key === 'ArrowUp' || e.key === 'w') &&
+        snake.moveDirection !== 'down'
+    ) {
+        snake.moveDirection = 'up';
+    } else if (
+        (e.key === 'ArrowLeft' || e.key === 'a') &&
+        snake.moveDirection !== 'right'
+    ) {
+        snake.moveDirection = 'left';
+    } else if (
+        (e.key === 'ArrowDown' || e.key === 's') &&
+        snake.moveDirection !== 'up'
+    ) {
+        snake.moveDirection = 'down';
+    } else if (
+        (e.key === 'ArrowRight' || e.key === 'd') &&
+        snake.moveDirection !== 'left'
+    ) {
+        snake.moveDirection = 'right';
+    }
+}
+
 export function checkControl() {
-    document.addEventListener('keydown', (e) => {
-        if ((e.key === 'ArrowUp' || e.key === 'w') && snake.moveDirection !== 'down') {
-            snake.moveDirection = 'up';
-        } else if ((e.key === 'ArrowLeft' || e.key === 'a') && snake.moveDirection !== 'right') {
-            snake.moveDirection = 'left';
-        } else if ((e.key === 'ArrowDown' || e.key === 's') && snake.moveDirection !== 'up') {
-            snake.moveDirection = 'down';
-        } else if ((e.key === 'ArrowRight' || e.key === 'd') && snake.moveDirection !== 'left') {
-            snake.moveDirection = 'right';
-        }
-    }, { once : true });
+    document.addEventListener('keydown', eventHandler, { once: true });
 }
 
 document.addEventListener('keydown', (e) => {
-    if ((e.key === 'Escape' || e.key === 'p') && !game.isPaused && game.isActive) {
+    if (
+        (e.key === 'Escape' || e.key === 'p') &&
+        !game.isPaused &&
+        game.isActive
+    ) {
         game.pause();
     } else if ((e.key === 'Escape' || e.key === 'p') && game.isPaused) {
         game.unpause();
